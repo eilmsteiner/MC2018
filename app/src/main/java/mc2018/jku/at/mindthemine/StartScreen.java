0bbfab8a9ef5d643be705afbbdaf876626372229
DWave3239 <marvin.hengster@gmx.at>
2018-11-27 14:39:00 +0100

    First playable variant (really now). Settings loading doesn't work yet.

--- app/src/main/java/mc2018/jku/at/mindthemine/StartScreen.java
+++ app/src/main/java/mc2018/jku/at/mindthemine/StartScreen.java
@@ -1,7 +1,11 @@
 package mc2018.jku.at.mindthemine;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class StartScreen extends AppCompatActivity {
 
@@ -9,5 +13,39 @@ public class StartScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        Button sp = findViewById(R.id.btnSinglePlayer);
+        sp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent toSinglePlayer = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(toSinglePlayer);
+            }
+        });
+
+        Button coopMp = findViewById(R.id.btnCoopMP);
+        coopMp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "To be implemented!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button compMp = findViewById(R.id.btnCompMP);
+        compMp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "To be implemented!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button settings = findViewById(R.id.btnSettings);
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toSettings = new Intent(getBaseContext(), SettingsActivity.class);
+                startActivity(toSettings);
+            }
+        });
     }
 }
