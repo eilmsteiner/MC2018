47233b14394e25e2425e6ba74fb856950c32af64
Christian Mayrhofer <chris.mayrhofer@aon.at>
2018-12-04 14:16:53 +0100

    added basic location implementation and location accuracy indicator

--- app/src/main/java/mc2018/jku/at/mindthemine/MainActivity.java
+++ app/src/main/java/mc2018/jku/at/mindthemine/MainActivity.java
@@ -1,15 +1,25 @@
 package mc2018.jku.at.mindthemine;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.drawable.GradientDrawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
+import android.os.SystemClock;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +28,7 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.Chronometer;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -44,10 +55,57 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView remainingCounter;
 
+    private LocationManager mLocationManager_gps;
+    private LocationManager mLocationManager_net;
+    private static int LOCATION_REFRESH_DISTANCE = 2000;
+    private static int LOCATION_REFRESH_TIME = 1;
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION_FINE = 2;
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION_COARSE = 1;
+    private String provider_gps = LocationManager.GPS_PROVIDER;
+    private String provider_net = LocationManager.NETWORK_PROVIDER;
+    private Chronometer cmTimer;
+    private TextView txtAcc;
+    private Button btnIndicator;
+    private int timeShow;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initialize the whole location stuff
+
+        mLocationManager_gps = (LocationManager) getSystemService(LOCATION_SERVICE);
+        mLocationManager_net = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        while (!checkLocationPermission()) ;
+
+        mLocationManager_gps.requestLocationUpdates(provider_gps, LOCATION_REFRESH_TIME,
+                LOCATION_REFRESH_DISTANCE, mLocationListener);
+        mLocationManager_net.requestLocationUpdates(provider_net, LOCATION_REFRESH_TIME,
+                LOCATION_REFRESH_DISTANCE, mLocationListener);
+
+        cmTimer = findViewById(R.id.txtAge);
+        cmTimer.setText("Last location update:    0 seconds ago");
+
+        txtAcc = findViewById(R.id.txtAcc);
+        btnIndicator = findViewById(R.id.btnIndicator);
+        timeShow = 0;
+
+        cmTimer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            public void onChronometerTick(Chronometer arg0) {
+                long time = SystemClock.elapsedRealtime() - cmTimer.getBase();
+                int h = (int) (time / 3600000);
+                int m = (int) (time - h * 3600000) / 60000;
+                int s = (int) (time - h * 3600000 - m * 60000) / 1000;
+
+                if(s%5 == 0)
+                    timeShow+=5;
+                cmTimer.setText(String.format("Last location update:  < %3d seconds ago", timeShow+5));
+            }
+        });
+
+        // minesweeper-specific stuff
 
         TableLayout tl = findViewById(R.id.table);
         remainingCounter = findViewById(R.id.remaingCounter);
@@ -201,6 +259,40 @@ public class MainActivity extends AppCompatActivity {
         //Toast.makeText(getBaseContext(), "DONE", Toast.LENGTH_SHORT).show();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            mLocationManager_gps.requestLocationUpdates(provider_gps, 400, 1, mLocationListener);
+        }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            mLocationManager_gps.requestLocationUpdates(provider_net, 400, 1, mLocationListener);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            mLocationManager_gps.removeUpdates(mLocationListener);
+        }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            mLocationManager_gps.requestLocationUpdates(provider_net, 400, 1, mLocationListener);
+        }
+    }
+
     private int convertToDp(int pixelValue) {
         float scale = getResources().getDisplayMetrics().density;
         return (int) (pixelValue * scale + 0.5f);
@@ -325,5 +417,186 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return iconColor;
+    }
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+            //your code here
+            Location pos = new Location("");
+            pos.setLatitude(location.getLatitude());
+            pos.setLongitude(location.getLongitude());
+
+            float acc = location.getAccuracy();
+            txtAcc.setText(String.format("%.1f", acc));
+
+            updateIndicator(btnIndicator, acc);
+
+            cmTimer.setBase(SystemClock.elapsedRealtime());
+            cmTimer.start();
+            cmTimer.setText("Last location update:     0 seconds ago");
+            timeShow = 0;
+
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+
+    };
+
+    private void updateIndicator(Button btnIndicator, float acc) {
+        if(acc > 10)
+            btnIndicator.setBackgroundColor(getResources().getColor(android.R.color.holo_red_dark,this.getTheme()));
+        else if (acc > 5)
+            btnIndicator.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light,this.getTheme()));
+        else
+            btnIndicator.setBackgroundColor(getResources().getColor(android.R.color.holo_green_light,this.getTheme()));
+
+//          for testing during development
+//        if (acc > 30)
+//            btnIndicator.setBackgroundColor(getResources().getColor(android.R.color.holo_red_dark, this.getTheme()));
+//        else if (acc > 25)
+//            btnIndicator.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light, this.getTheme()));
+//        else
+//            btnIndicator.setBackgroundColor(getResources().getColor(android.R.color.holo_green_light, this.getTheme()));
+
+    }
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.app_name)
+                        .setMessage(R.string.app_name)
+                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION_FINE);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION_FINE);
+            }
+            return false;
+        }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.app_name)
+                        .setMessage(R.string.app_name)
+                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION_COARSE);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION_COARSE);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION_FINE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+                        //Request location updates:
+                        mLocationManager_gps.requestLocationUpdates(provider_gps, 400, 1, mLocationListener);
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+
+                }
+                return;
+            }
+            case MY_PERMISSIONS_REQUEST_LOCATION_COARSE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+                        //Request location updates:
+                        mLocationManager_net.requestLocationUpdates(provider_net, 400, 1, mLocationListener);
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+
+                }
+                return;
+            }
+
+        }
     }
 }
